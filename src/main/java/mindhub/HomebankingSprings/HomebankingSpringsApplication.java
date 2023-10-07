f10aa582ff6594703fc1e496373e4bdcc9580378
jennys jhoana guzman martinez <jhoana030guzman@gmail.com>
2023-10-07 10:45:03 -0500

    mi primer commit

---
+++ src/main/java/mindhub/HomebankingSprings/HomebankingSpringsApplication.java
@@ -0,0 +1,13 @@
+package mindhub.HomebankingSprings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebankingSpringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebankingSpringsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mindhub/HomebankingSprings/HomebankingSpringsApplicationTests.java
@@ -0,0 +1,13 @@
+package mindhub.HomebankingSprings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomebankingSpringsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
